76b847339f05cf8ec4ad991690096b5aa92fff2a
thomas <thomas.broussard@local.local>
2020-10-23 12:44:33 +0200

    * sample of JPA DAO

---
+++ quiz-core/src/main/java/fr/epita/services/QuestionJPADAO.java
@@ -0,0 +1,22 @@
+package fr.epita.services;
+
+import javax.inject.Inject;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import fr.epita.datamodel.Question;
+
+public class QuestionJPADAO {
+
+	@Inject
+	SessionFactory sf;
+
+	public void create(Question question) {
+		Session session = sf.openSession();
+		session.save(question);
+	}
+	
+	
+
+}
