db0a183c10e27a0abf8f65e8be469168b3db3d50
thomas <thomas.broussard@local.local>
2020-10-26 11:28:17 +0100

    * ManyToOne addition

--- quiz-core/src/main/java/fr/epita/datamodel/MCQChoice.java
+++ quiz-core/src/main/java/fr/epita/datamodel/MCQChoice.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -17,7 +18,7 @@ public class MCQChoice {
 	private String choice;
 	private boolean valid;
 	
-
+	@ManyToOne
 	private Question question;
 	
 	public String getChoice() {
